3769aaeafe682f1a0d6d29b6faaa27cef1aac7d3
Mjavaidnawab <57194178+Mjavaidnawab@users.noreply.github.com>
2020-08-21 15:39:47 +0400

    update display msg

--- src/App.js
+++ src/App.js
@@ -4,21 +4,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Juma 21-08-2020 Hello Urdu Assalam o Alaikum Mustafa Maryam
     </div>
   );
 }
